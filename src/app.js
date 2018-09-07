5dc8d2e4014ec6cd979d54abc0ef2062bd4661f5
Carlsiry <chenkansheng0@163.com>
2018-09-07 22:58:23 +0800

    基本完成文件与文件夹的展示

--- src/app.js
+++ src/app.js
@@ -1,5 +1,48 @@
 
-function name(params) {
-  console.error(params)
-}
-name()
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const handlebars = require('handlebars')
+const promisify = require('util').promisify
+
+const conf = require('./config/default')
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
+
+const rootDir = conf.root
+const tplPath = path.join(__dirname, './template/dir.tpl')
+const source = fs.readFileSync(tplPath, 'utf8')
+const template = handlebars.compile(source)
+
+const server = http.createServer(async (req, res) => {
+  const url = path.join(rootDir, req.url)
+  res.statusCode = 200
+  try {
+    const status = await stat(url)
+    if (status.isFile()) {
+      res.setHeader('Content-Type', 'text/plain')
+      fs.createReadStream(url).pipe(res)
+    } else if(status.isDirectory()) {
+      res.setHeader('Content-Type', 'text/html')
+      const files = await readdir(url)
+      // const dir = path.relative(rootDir, url)
+      const data = {
+        title: path.basename(url),
+        // dir: dir ? '/' + dir : '',
+        dir: req.url === '/' ? '' : req.url,
+        files,
+      }
+      res.end(template(data))
+    }
+  } catch (error) {
+    res.statusCode = 404
+    res.end('file or directory not found \n' + error)
+  }
+})
+
+const hostname= conf.hostName
+const port = conf.port
+
+server.listen(port, hostname, () => {
+  console.info(`server running at http://${hostname}:${port}/`)
+})
---
+++ src/config/default.js
@@ -0,0 +1,7 @@
+
+
+module.exports = {
+  root: process.cwd(),
+  hostName: '127.0.0.1',
+  port: 3000,
+}
